56e2214c04bf607aaf443aa2eae98c78384506d8
MacStudent <macstudent@128-imac16.local>
2018-11-22 09:44:43 -0500

    login signup code

--- InClassExercisesStarter/LoginViewController.swift
+++ InClassExercisesStarter/LoginViewController.swift
@@ -5,37 +5,92 @@
 //  Created by parrot on 2018-11-22.
 //  Copyright © 2018 room1. All rights reserved.
 //
+ import UIKit
+ import FirebaseAuth
 
-import UIKit
-
-class LoginViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+ class LoginViewController: UIViewController {
     
-
     
-    // MARK: Actions
+    @IBOutlet weak var txtEmail: UITextField!
+  
+    @IBOutlet weak var txtPassword: UITextField!
+    var email = ""
+    var password = ""
+   
+    @IBOutlet weak var txtMessage: UILabel!
     
-    @IBAction func loginButtonPressed(_ sender: Any) {
-        print("Pressed login button")
+  
+ 
+    @IBAction func btnLogin(_ sender: Any) {
         
-        // HINT:  The name of the segue that goes to the next screen is: segueLoginSignup
-        // You can check the name by going to Main.storyboard > clicking on segue > looking at Attributes Inspector
+        email = txtEmail.text!
+        password  = txtPassword.text!
+        
+        Auth.auth().signIn(withEmail: email, password: password) {
+            
+            (user, error) in
+            
+            if (user != nil) {
+                // 1. Found a user!
+                print("User signed in ")
+                print("User id: \(user?.user.uid)")
+                print("Email: \(user?.user.email)")
+                
+                // 2. So send them to screen 2!
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            else {
+                // 1. A problem occured when looking up  the user
+                // - doesn't meet password requirements
+                // - user already exists
+                print("ERROR!")
+                print(error?.localizedDescription)
+                
+                self.txtMessage.text = error?.localizedDescription
+                
+                // 2. Show the error in user interface
+                // self.statusMessageLabel.text = error?.localizedDescription
+            }
+        }
+        
+        
     }
     
     
     @IBAction func signupButtonPressed(_ sender: Any) {
         print("pressed signup button")
-
+        email = txtEmail.text!
+        password  = txtPassword.text!
+        Auth.auth().createUser(withEmail: email, password: password) {
+            
+            (user, error) in
+            
+            if (user != nil) {
+                // 1. New user created!
+                print("Created user: ")
+                print("User id: \(user?.user.uid)")
+                print("Email: \(user?.user.email)")
+                
+                //2. @TODO: You decide what you want to do next!
+                // - do you want to send them to the next page?
+                // - maybe ask them to fill in other forms?
+                // - show a tutorial?
+                
+                self.txtMessage.text = "Account is Created."
+            
+                
+            }
+            else {
+                // 1. Error when creating a user
+                print("ERROR!")
+                print(error?.localizedDescription)
+                
+                // 2. Show the error in the UI
+                self.txtMessage.text = error?.localizedDescription
+                
+            }
+        }
+    }
         // HINT:  The name of the segue that goes to the next screen is: segueLoginSignup
         // You can check the name by going to Main.storyboard > clicking on segue > looking at Attributes Inspector
         
@@ -51,4 +106,3 @@ class LoginViewController: UIViewController {
     }
     */
 
-}
